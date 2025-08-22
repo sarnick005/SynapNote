3b301a3fbd6aa2fa407ebd4677e39039ad07e06d
sarnick005 <sarnick.chakraborty@gmail.com>
2025-08-22 22:19:16 +0530

    initialized nextjs, tailwind and prisma

---
+++ lib/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@/lib/generated/prisma";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prismaClient = globalThis.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismaClient;
+
+export default prismaClient;
